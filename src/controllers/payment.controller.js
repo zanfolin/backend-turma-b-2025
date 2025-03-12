86010cf9e48efa4a2a3e9a33636bff486ce67393
Graziani Vasconcelos Zanfolin <54035931+zanfolin@users.noreply.github.com>
2025-03-12 13:30:02 +0000

    Payment Validation  Zod

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -3,6 +3,14 @@
  * do banco de dados, por exemplo, se temos uma entidade
  * chamada Payment, devemos criar um PaymentController.
  */
+import {z} from "zod";
+
+const PaymentSchema = z.object({
+    data: z.string().datetime({message: 'Data inválida'}),
+    recibo: z.number({message: 'Recibo inválido'}).min(1,{message: 'Recibo inválido'}),
+    valor: z.number({message: 'Valor inválido'}).min(0,{message: 'Valor inválido'}),
+    observacao: z.string().optional(),
+});
 
 const PaymentController = {
     /**
@@ -14,9 +22,14 @@ const PaymentController = {
     async createPayment(req,res){
         try {
             const {data, recibo, valor, observacao} = req.body;
+            PaymentSchema.parse({data, recibo, valor, observacao});
             return res.status(201).json({message: 'Payment created',
                                          data: {data, recibo, valor, observacao}});
         } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({message: "Validation error", details: error.errors});
+            }
+
             return res.status(500).json({message: 'Internal server error'});
         }
     },
